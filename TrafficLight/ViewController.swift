d106ec18d46d54ce2fd5b9db0a1c0601843cf727
Shaqra University <ccitlms@gmail.com>
2022-03-06 12:15:00 +0300

    View Controller Code!

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func toYellow(_ sender: Any) {
+        if redSwitch.isOn
+        {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func toGreen(_ sender: Any) {
+        if redSwitch.isOn
+        {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
 
-
+    }
+    
 }
 
